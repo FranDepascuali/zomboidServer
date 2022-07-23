a7a49fc54320ecfc227242abb378eef9822ab764
Fran Depa <depa.developer@gmail.com>
2022-07-23 21:52:51 +0000

    Add spawn points

--- servertest_spawnregions.lua
+++ servertest_spawnregions.lua
@@ -4,6 +4,7 @@ function SpawnRegions()
 		{ name = "West Point, KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
 		{ name = "Rosewood, KY", file = "media/maps/Rosewood, KY/spawnpoints.lua" },
 		{ name = "Riverside, KY", file = "media/maps/Riverside, KY/spawnpoints.lua" },
-		{ name = "RavenCreek, KY", file = "media/maps/RavenCreek, KY/spawnpoints.lua" }
+	        { name = "RavenCreek", file = "media/maps/RavenCreek/spawnpoints.lua" },
+		{ name = "The Walking Dead Project-Pack", file = "media/maps/The Walking Dead Project-Pack/spawnpoints.lua" },
 	}
 end
